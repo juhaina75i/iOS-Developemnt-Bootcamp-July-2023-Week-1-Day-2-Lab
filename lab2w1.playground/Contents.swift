758452abad4d4e2ac7d72971561813aa55fa5e12
Juhaina Alrashidi <132265687+juhaina75i@users.noreply.github.com>
2023-08-02 11:00:15 +0300

    Add files via upload

---
+++ lab2w1.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var score: Int = 2
+
+score += 10
+print(score)
+
+if score > 9 {
+    print("Pass")
+} else {
+    print("Fail")
+}
+
+var arr = ["Juhaina", "Sara","Layan","Sadeem","Norah"]
+
+for i in arr {
+   print(i)
+}
+
+var fruit = ["apple":3, "orange":2, "banana":1,"kiwi":2, "peach":7]
+
+for i in fruit {
+    print(i)
+}
+
+let name = "Juhaina "
+print("Welcome \(name)")
